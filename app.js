3c6d8da75fc67227c6b77ecbe6f7818940d23a4d
Richard Hai <hirichardhai@gmail.com>
2018-08-26 23:53:35 -0700

    added v-model to demonstrate two way data binding

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ new Vue({
       eventDate: 'August 14th - 16th',
       eventTitle: 'Summer Festival!',
       eventDescription: "It's back! This years summer festival will be in the beautiful countryside featuring our best line up ever!"
-    }
+    },
+    newNameText: '',
+    guestName: []
   }
 });
