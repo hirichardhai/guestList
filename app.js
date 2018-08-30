de078376f01b9da36f3265702406d4d837c4e014
hirichardhai <hirichardhai@gmail.com>
2018-08-29 17:12:03 -0700

    added v-text, v-html, v-once for practice and application

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ new Vue({
     event: {
       eventDate: 'August 14th - 16th',
       eventTitle: 'Summer Festival!',
-      eventDescription: "It's back! This years summer festival will be in the beautiful countryside featuring our best line up ever!"
+      eventDescription: "It's back! This years summer festival will be in the beautiful countryside featuring our best line up ever!",
+      signUpText: 'Add your name to the guest list for <em>exclusive</em> offers:'
     },
     newNameText: '',
     guestName: [],
